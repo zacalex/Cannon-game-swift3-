f27d9164c330976c6c3f7572ca704af152679f95
zacalex <qinjiahuangus@gmail.com>
2017-04-20 00:57:35 -0700

    first commit

---
+++ SpriteKitinitro/MainMenu.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenu.swift
+//  SpriteKitinitro
+//
+//  Created by Qinjia Huang on 4/19/17.
+//  Copyright © 2017 Qinjia Huang. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: SKScene {
+
+}
